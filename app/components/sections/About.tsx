d09b3c7aa928ef0dfa934d6701d80e201616b20d
Mahmoud-Fattah <hoodafatah@gmail.com>
2026-08-17 18:43:31 +0300

    update about to inhance the performace

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -215,7 +215,18 @@ useEffect(() => {
       ref={containerRef}
       className="relative mb-4 w-full min-h-dvh overflow-hidden bg-(--color-bg-soft)"
     >
-      <div className="container mx-auto min-h-screen max-w-6xl flex flex-col items-center justify-center gap-8 px-4 relative z-10">
+      <div className="  container
+    mx-auto
+    h-full
+    max-w-6xl
+    flex
+    flex-col
+    items-center
+    justify-center
+    gap-8
+    px-4
+    relative
+    z-10">
         {/* TITLE */}
         <h2 className="will-fade text-center text-4xl md:text-7xl font-extrabold leading-[1.2] text-gray-900 will-change-transform">
           شراء سيارات مصدومة
