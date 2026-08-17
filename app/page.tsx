fd4373f19001244e7b6822cf7e4459047028e1d8
Mahmoud-Fattah <hoodafatah@gmail.com>
2026-08-17 21:18:44 +0300

    test layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Brands />
-      {/* <CarBrandsSection/> */}
+      {/* <Brands />
       <About />
-      <OurApproach />
+      <OurApproach /> */}
       <ServicesGrid />
       <CitiesWeServe />
       <Contact />
+      {/* <CarBrandsSection/> */}
     </>
   );
 }
